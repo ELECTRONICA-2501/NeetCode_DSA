3e2b9b1f4a0bcbc4e39d429b75a6c994fa4704e2
E L E C T R O N I C A <mfsaljimenez@gmail.com>
2024-09-06 19:50:19 -0500

    Init COmmit

--- src/Hashing/LinearProbing.java
+++ src/Hashing/LinearProbing.java
@@ -53,16 +53,44 @@ public class LinearProbing {
         } else {
         int index = modASCIIHashFunction(word, hashTable.length); //calc new index
             for (int i = index; i < index + hashTable.length; i++){
-                int newIndex = i% (index+hashTable.length);
+                int newIndex = i % hashTable.length;
                 if (hashTable[newIndex] == null){
                     hashTable[newIndex] = word;
                     System.out.println("The " + word + " successfully inserted at location: " + newIndex);
-                    break;
+                    break; //this breaks us out of for loop
                 }else {
                     System.out.println(newIndex + " is already occupied, Trying next empty cell");
+                    //this has no break so we iterate again
                 }
             }
         }
         usedCellNumber++;
+    }
+
+    // -------------------------------search in HT
+    public boolean searchHashTable(String word){
+        int index = modASCIIHashFunction(word, hashTable.length);
+        for(int i = 0; i < index + hashTable.length; i++){
+            int newIndex = i % hashTable.length;
+            if (hashTable[newIndex] != null && hashTable[newIndex].equals(word)){
+                System.out.println(word + " found at location : " + newIndex);
+                return true;
+            }
+        }
+        System.out.println(word + "Not found in HT");
+        return false;
+    }
+    //------------------------------delete from HT
+    public void delete_KeyFromHashTable(String word){
+        int index = modASCIIHashFunction(word, hashTable.length); //compute index of the word/ key we want to delete
+        for(int i = index; i < index + hashTable.length; i++){ // loop through HT
+            int newIndex = i % hashTable.length;
+            if( hashTable[newIndex] != null && hashTable[newIndex].equals(word)){
+                hashTable[newIndex] = null;
+                System.out.println(word + " has been deleted from HashTable");
+                return;
+            }
+        }
+        System.out.println(word + " is not in HashTable ");
     }
 }
